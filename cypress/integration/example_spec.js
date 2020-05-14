0ee76a4c4cc187962273bc868d0adb9bdc07caf6
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2020-05-14 17:08:19 +0200

    Make sure dirs exist

--- cypress/integration/example_spec.js
+++
@@ -1,6 +0,0 @@
-describe('src/js/example.html', () => {
-  it('should render -1.00', () => {
-    cy.visit('http://localhost:8000/src/js/example.html');
-    cy.get('#answer').contains('-1.00');
-  });
-});
